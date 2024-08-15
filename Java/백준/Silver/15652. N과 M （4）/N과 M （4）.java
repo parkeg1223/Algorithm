716390d0e06776e7323d14743fcff6e92d45d45c
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-08-16 01:02:57 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 17196 KB -BaekjoonHub

---
+++ Java/백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] num;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        num = new int[M];
+        dfs(1, 0);
+        System.out.print(sb);
+    }
+
+    public static void dfs(int n, int k) {
+        if (k == M) {
+            for (int nu: num) {
+                sb.append(nu).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = n; i <= N; i++) {
+            num[k] = i;
+            dfs(i, k+1);
+        }
+    }
+}
