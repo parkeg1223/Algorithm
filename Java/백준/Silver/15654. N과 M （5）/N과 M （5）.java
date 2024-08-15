334161c5c6f2a7d1dde5278c5ed56d3ada2af224
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-08-16 01:09:27 +0900

    [Silver III] Title: N과 M (5), Time: 308 ms, Memory: 29040 KB -BaekjoonHub

---
+++ Java/백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] num, arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        num = new int[M];
+        arr = new int[N];
+        visited = new boolean[N];
+        st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.print(sb);
+    }
+
+    public static void dfs(int k) {
+        if (k == M) {
+            for (int nu: num) {
+                sb.append(nu).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                num[k] = arr[i];
+                visited[i] = true;
+                dfs(k+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
