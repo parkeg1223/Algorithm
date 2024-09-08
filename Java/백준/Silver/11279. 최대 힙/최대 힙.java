d879508a32b50bd3f4f5b3044fb3878443e0c39b
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-09-08 21:24:25 +0900

    [Silver II] Title: 최대 힙, Time: 288 ms, Memory: 25996 KB -BaekjoonHub

---
+++ Java/백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(in.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(in.readLine());
+            if (n == 0) {
+                if (pq.isEmpty()) sb.append(0).append("\n");
+                else sb.append(pq.poll()).append("\n");
+            } else {
+                pq.offer(n);
+            }
+        }
+        System.out.print(sb);
+    }
+}
