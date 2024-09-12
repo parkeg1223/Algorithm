afc52455a97daacdd6a4b3f4f8895fc77fede492
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-09-12 22:48:55 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 18796 ms, Memory: 129668 KB -BaekjoonHub

---
+++ Java/백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(in.readLine());
+        List<Integer> nList = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            nList.add(Integer.parseInt(in.readLine()));
+        }
+
+        Collections.sort(nList);
+        for (int n: nList) {
+            sb.append(n).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
