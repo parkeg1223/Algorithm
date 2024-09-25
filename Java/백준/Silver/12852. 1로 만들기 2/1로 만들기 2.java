9ac8651c058e855455d860cba04f1b37986cd1c8
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-09-25 09:52:37 +0900

    [Silver I] Title: 1로 만들기 2, Time: 160 ms, Memory: 22340 KB -BaekjoonHub

--- Java/백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ Java/백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -3,14 +3,16 @@ import java.util.*;
 
 public class Main {
 
-    static int[] ans = new int[1_000_001], prev = new int[1_000_001];
+    static int[] ans , prev;
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(in.readLine());
-        for (int i = 1; i <= 1_000_000; i++) {
-            if (i * 3 <= 1_000_000) {
+        ans = new int[N+1];
+        prev = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            if (i * 3 <= N) {
                 if (ans[i*3] == 0) {
                     ans[i*3] = ans[i] + 1;
                     prev[i*3] = i;
@@ -19,7 +21,7 @@ public class Main {
                     if (ans[i*3] == ans[i] + 1) prev[i*3] = i;
                 }
             }
-            if (i * 2 <= 1_000_000) {
+            if (i * 2 <= N) {
                 if (ans[i*2] == 0) {
                     ans[i*2] = ans[i] + 1;
                     prev[i*2] = i;
@@ -30,7 +32,7 @@ public class Main {
                 }
             }
 
-            if (i + 1 <= 1_000_000) {
+            if (i + 1 <= N) {
                 if (ans[i+1] == 0) {
                     ans[i+1] = ans[i] + 1;
                     prev[i+1] = i;
@@ -42,10 +44,9 @@ public class Main {
         }
 
         sb.append(ans[N]).append("\n");
-        int num = N;
-        while (num > 0) {
-            sb.append(num).append(" ");
-            num = prev[num];
+        while (N > 0) {
+            sb.append(N).append(" ");
+            N = prev[N];
         }
         System.out.println(sb);
     }
