5eef96dccbf41602bd3614ae196be5e14cb0ae28
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-09-25 10:39:29 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 108 ms, Memory: 14184 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static long[] ans;
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(in.readLine());
+        ans = new long[n+1];
+        ans[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            ans[i] = ans[i-1] + ans[i-2];
+        }
+        System.out.println(ans[n]);
+    }
+}
