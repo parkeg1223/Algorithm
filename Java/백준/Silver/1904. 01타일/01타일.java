22e9dc9da045c776f23dcaf9955df350e62d806d
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-09-27 20:20:26 +0900

    [Silver III] Title: 01타일, Time: 116 ms, Memory: 18080 KB -BaekjoonHub

---
+++ Java/백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+
+        int[] dp = new int[N+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 15746;
+        }
+        System.out.println(dp[N]);
+    }
+}
