31f7eec3bb213f6a538a5458fed67059a4358cf7
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-10-14 21:24:56 +0900

    [Silver II] Title: 주식, Time: 1024 ms, Memory: 314276 KB -BaekjoonHub

--- Java/백준/Silver/11501. 주식/주식.java
+++ Java/백준/Silver/11501. 주식/주식.java
@@ -13,31 +13,16 @@ public class Main {
         while (T-- > 0) {
             N = Integer.parseInt(in.readLine());
             int[] nArr = new int[N];
-            List<int[]> nList = new ArrayList<>();
             StringTokenizer st = new StringTokenizer(in.readLine());
             for (int i = 0; i < N; i++) {
-                int num = Integer.parseInt(st.nextToken());
-                nArr[i] = num;
-                nList.add(new int[] {i, num});
+                nArr[i] = Integer.parseInt(st.nextToken());
             }
 
-            nList.sort((o1, o2) -> {
-                if (o1[1] == o2[1]) return o1[0] - o2[0];
-                return o2[1] - o1[1];
-            });
-
-            int idx = 0, curIdx;
-            for (int i = 0; i < nList.size(); i++) {
-                int[] curMax = nList.get(i);
-                curIdx = curMax[0];
-                if (curIdx < idx) continue;
-                for (int j = idx; j < curIdx; j++) {
-                    answer += curMax[1] - nArr[j];
-                }
-                idx = curIdx + 1;
-                if (idx == nList.size()) break;
+            int curMax = 0;
+            for (int i = N-1; i >= 0; i--) {
+                if (nArr[i] > curMax) curMax = nArr[i];
+                else answer += curMax - nArr[i];
             }
-
             sb.append(answer).append("\n");
             answer = 0;
         }
