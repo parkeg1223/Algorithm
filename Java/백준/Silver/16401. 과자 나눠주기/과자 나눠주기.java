e5f70894591a2bf9faf5b95dbcf572b990881c95
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-10-24 02:17:46 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1096 ms, Memory: 124612 KB -BaekjoonHub

---
+++ Java/백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int m, n;
+    static int[] snack;
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        snack = new int[n];
+        st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < n; i++) {
+            snack[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(snack);
+        int start = 1, end = snack[n-1], mid, answer = 0;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (possible(mid)) {
+                start = mid+1;
+                answer = Math.max(answer, mid);
+            }
+            else end = mid-1;
+        }
+        System.out.println(answer);
+    }
+
+    public static boolean possible(int num) {
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            temp += snack[i] / num;
+        }
+        return temp >= m;
+    }
+}
