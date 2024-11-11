90fa6f6ed4ed10e27dae5444e47ce6fec5d0d45a
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-11-11 15:34:39 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 112 ms, Memory: 14724 KB -BaekjoonHub

---
+++ Java/백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        ArrayDeque<Integer> waiting = new ArrayDeque<>();
+        ArrayDeque<Integer> space = new ArrayDeque<>();
+
+        int N = Integer.parseInt(in.readLine());
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < N; i++) {
+            waiting.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int next = 1;
+        while (next <= N) {
+            if (!waiting.isEmpty()) {
+                if (waiting.peek() == next) {
+                    waiting.poll();
+                    next++;
+                } else if (!space.isEmpty() && space.peek() == next) {
+                    space.poll();
+                    next++;
+                } else {
+                    space.addFirst(waiting.poll());
+                }
+            } else if (!space.isEmpty() && space.peek() == next) {
+               space.poll();
+               next++;
+            } else {
+                System.out.println("Sad");
+                return;
+            }
+        }
+        System.out.println("Nice");
+    }
+}
