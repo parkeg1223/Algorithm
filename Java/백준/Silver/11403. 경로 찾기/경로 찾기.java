4d14accd8327efd7bdb8dd9da2ae1e1df3d956a5
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-11-15 18:18:01 +0900

    [Silver I] Title: 경로 찾기, Time: 176 ms, Memory: 17144 KB -BaekjoonHub

---
+++ Java/백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int N = Integer.parseInt(in.readLine());
+        int[][] ans = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(in.readLine());
+            for (int j = 0; j < N; j++) {
+                ans[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (ans[i][j] == 1) continue;
+                    if (ans[i][k] == 1 && ans[k][j] == 1) ans[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(ans[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
